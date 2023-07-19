466bcb79a0b7fcaae3012836fae612bc1c5f30b6
choibumku00 <101037541+choibumku00@users.noreply.github.com>
2023-07-19 20:39:46 +0900

    feat: gpt응답 출력 완료

--- src/main/resources/static/js/gptForm.js
+++ src/main/resources/static/js/gptForm.js
@@ -1,11 +1,12 @@
 // jQuery code (optional, for some interactive features if needed)
 $(document).ready(function () {
     let call_btn = $('#start-call');
-    call_btn.onclick = function (event) {
+    call_btn.click(function (event) {
+        console.log('클릭함수 진입');
         event.preventDefault(); // 폼 제출 방지
         let difficulty_val = $('input[name="difficulty"]:checked').val();
-        let ingredient_val = document.querySelector('#myText').value;
-
+        let ingredient_val = $('#ingredients').val();
+        $("#loading").fadeIn(200);
         // AJAX 요청
         $.ajax({
             type: 'POST',
@@ -15,8 +16,11 @@ $(document).ready(function () {
                 difficulty: difficulty_val }), // JSON 형식으로 데이터 전송
             success: function (result) {
                 if (result) {
-                    alert('값을 받아옴');
-                    console.log(result);
+                    let markGptResponse = marked(result.gptResponse);
+                    $("#loading").fadeOut(200, function () {
+                        $('#result-container').fadeIn(500);
+                        $("#result-content").html(markGptResponse);
+                    })
                 } else {
                     alert('값받기 실패');
                 }
@@ -24,35 +28,6 @@ $(document).ready(function () {
             error: function () {
                 alert('서버 오류가 발생했습니다.');
             },
-        });
-        var markdownResult = "# Recipe Title\n\n- Ingredient 1\n- Ingredient 2\n- Ingredient 3\n";
-        var htmlResult = marked(markdownResult); // Convert markdown to HTML
-        $("#loading").fadeOut(200, function () {
-            $("#result-container").html(htmlResult).fadeIn(500);
-        });
-    };
-
-    $("#recipe-form").submit(function (event) {
-        event.preventDefault();
-        $("#recipe-form").fadeOut(500, function () {
-            $("#loading").fadeIn(300, function () {
-                // Simulate server response time (you should replace this with actual API call)
-                setTimeout(function () {
-                    // Here, you can call your server API to get the markdown result
-                    // For demonstration purposes, let's assume the result is in a variable called 'markdownResult'
-                    var markdownResult = "# Recipe Title\n\n- Ingredient 1\n- Ingredient 2\n- Ingredient 3\n";
-                    var htmlResult = marked(markdownResult); // Convert markdown to HTML
-                    $("#loading").fadeOut(200, function () {
-                        $("#result-container").html(htmlResult).fadeIn(500);
-                    });
-                }, 2000); // Simulate 2 seconds server response time
-            });
-        });
-
-    });
-    $("#replay-button").click(function () {
-        $("#result-container").fadeOut(500, function () {
-            $("#recipe-form").fadeIn(500);
         });
     });
 });
